ba6c62c6226e7fa9735600b69aca10af0efbf821
k1nderwunder <maxday202020@gmail.com>
2024-12-19 21:23:28 +0700

    2.2 - Разворот матрицы на 90 градусов (по часовой)
    Вариант 3

--- 9-11.py
+++ 9-11.py
@@ -23,6 +23,9 @@ def big_minus(mas_a, mas_b):
 def rotate90(matrix):
     return [list(reversed(col)) for col in zip(*matrix)]
 
+def rotate270(matrix):
+    return [list(row) for row in list(zip(*matrix))[::-1]]    
+
 if __name__ == "__main__":
     a = [[2, 3], [3, 4]]
-    print(rotate90(a))
+    print(rotate270(a))
