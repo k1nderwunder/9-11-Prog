428b97caa11254b6ea9cdab1017d24482ee8b4e1
k1nderwunder <maxday202020@gmail.com>
2024-12-19 21:17:06 +0700

    1.2 - Разность массивов с большими числами
    Вариант 4

--- 9-11.py
+++ 9-11.py
@@ -8,8 +8,20 @@ def big_sum(mas_a, mas_b):
         x //= 10
     return mas_x
 
+
+def big_minus(mas_a, mas_b):
+    int_mas_a = int(''.join(map(str, mas_a)))
+    int_mas_b = int(''.join(map(str, mas_b)))
+    z = int_mas_a - int_mas_b
+    mas_x = []
+    while z > 0:
+        mas_x.append(z % 10)
+        z //= 10
+    mas_x.reverse()
+    return mas_x
+
 if __name__ == "__main__":
     x = [2, 3, 4]
     y = [4, 3, 2]
-    print(big_sum(x,y))
-    print(432 + 234)
+    print(big_minus(y,x))
+    print(432 - 234)
